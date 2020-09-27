8c7ad0f982129d33e9cdb329b28a4d31cd9b0b1d
amirhossein-mohammadpoor <mohammadpoor104@gmail.com>
2020-09-28 00:53:20 +0330

    add redux reducers

---
+++ src/redux/reducers/cartReducer.js
@@ -0,0 +1,94 @@
+import types from '../types/cartTypes'
+
+if (!localStorage.getItem("carts")) {
+  localStorage.setItem("carts", JSON.stringify([]))
+}
+
+const initialState = {
+  carts: JSON.parse(localStorage.getItem("carts"))
+}
+
+const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    case types.CREATE_CART:
+      return {
+        ...state,
+        carts: [...state.carts, action.cart]
+      }
+    case types.ADD_ITEM:
+      return {
+        ...state,
+        carts: state.carts.map(c => (
+          c.email === action.cart.email ?
+            {
+              email: action.cart.email,
+              products: [...c.products, action.cart.products[0]]
+            } : c
+        ))
+      }
+    case types.INCREMENT_ITEM:
+      return {
+        ...state,
+        carts: state.carts.map(c => {
+          return c.email === action.email ?
+            {
+              email: action.email,
+              products: c.products.map(p => {
+                return p.productId === action.id ?
+                  {
+                    productId: p.productId,
+                    productName: p.productName,
+                    productPrice: p.productPrice,
+                    number: p.number + 1
+                  } : p
+              })
+            } : c
+        })
+      }
+    case types.DECREMENT_ITEM:
+      return {
+        ...state,
+        carts: state.carts.map(c => {
+          return c.email === action.email ?
+            {
+              email: action.email,
+              products: c.products.map(p => {
+                return p.productId === action.id ?
+                  {
+                    productId: p.productId,
+                    productName: p.productName,
+                    productPrice: p.productPrice,
+                    number: p.number - 1
+                  } : p
+              })
+            } : c
+        })
+      }
+    case types.REMOVE_ITEM:
+      return {
+        ...state,
+        carts: state.carts.map(c => {
+          return c.email === action.email ?
+            {
+              email: action.email,
+              products: c.products.filter(p => p.productId !== action.id)
+            } : c
+        })
+      }
+    case types.EMPTY_CART:
+      return {
+        ...state,
+        carts: state.carts.map(c => {
+          return c.email === action.email ?
+            {
+              ...c,
+              products: []
+            } : c
+        })
+      }
+    default:
+      return state
+  }
+}
+
+export default reducer
---
+++ src/redux/reducers/userReducer.js
@@ -0,0 +1,83 @@
+import types from "../types/userTypes"
+
+if (!localStorage.getItem("users")) {
+  localStorage.setItem("users", JSON.stringify([]))
+  localStorage.setItem("token", "")
+}
+
+const initialState = {
+  users: JSON.parse(localStorage.getItem("users")),
+  token: localStorage.getItem("token")
+}
+
+const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    case types.SIGNUP_USER:
+      return {
+        ...state,
+        users: [...state.users, action.user],
+        token: action.user.email
+      }
+    case types.SIGNOUT_USER:
+      return {
+        ...state,
+        token: ""
+      }
+    case types.SIGNIN_USER:
+      return {
+        ...state,
+        token: action.email
+      }
+    case types.EDIT_USER_PROFILE:
+      return {
+        ...state,
+        users: state.users.map(user => (
+          user.email === action.user.email ? action.user : user
+        ))
+      }
+    case types.USER_ADD_CREDIT:
+      return {
+        ...state
+      }
+    case types.SUBMIT_ORDER:
+      return {
+        ...state,
+        users: state.users.map(user => {
+          return user.email === action.email ?
+            {
+              ...user,
+              orders: [...user.orders, action.order],
+              credit: user.credit - action.price
+            } : user
+        })
+      }
+    case types.ADD_TO_FAVORITES:
+      return {
+        ...state,
+        users: state.users.map(user => {
+          return user.email === action.email ? 
+            {
+              ...user,
+              favorites: [...user.favorites, action.id]
+            }
+          : user
+        })
+      }
+    case types.REMOVE_FROM_FAVORITES:
+        return {
+          ...state,
+          users: state.users.map(user => {
+            return user.email === action.email ? 
+              {
+                ...user,
+                favorites: user.favorites.filter(f => f !== action.id)
+              }
+            : user
+          })
+        }
+    default:
+      return state
+  }
+}
+
+export default reducer
