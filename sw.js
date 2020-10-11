8bd5a4de314921e76776a0bacb5cd7d3e6bd1eb3
amirhossein-mohammadpoor <mohammadpoor104@gmail.com>
2020-10-11 18:46:37 +0330

    add delete-cache to service-worker

--- sw.js
+++ sw.js
@@ -2,9 +2,20 @@ import { precacheAndRoute } from 'workbox-precaching'
 import { registerRoute } from 'workbox-routing'
 import { StaleWhileRevalidate } from 'workbox-strategies'
 import { ExpirationPlugin } from 'workbox-expiration'
+import { setCacheNameDetails } from 'workbox-core'
 
-const CAROUSEL_CACHE = "carousel"
-const FONT_CACHE = "fonts"
+const PRECACHE_PREFIX = "node-market"
+const PRECACHE = "cache"
+const PRECACHE_VERSION = "v1"
+const PRECACHE_NAME = `${PRECACHE_PREFIX}-${PRECACHE}-${PRECACHE_VERSION}`
+const CAROUSEL_CACHE = "carousel-v2"
+const FONT_CACHE = "fonts-v2"
+
+setCacheNameDetails({
+  prefix: PRECACHE_PREFIX,
+  suffix: PRECACHE_VERSION,
+  precache: PRECACHE
+})
 
 precacheAndRoute(self.__WB_MANIFEST)
 
@@ -37,4 +48,24 @@ self.addEventListener("message", () => {
   if (event.data && event.data.type === 'SKIP_WAITING') {
     self.skipWaiting();
   }
+})
+
+
+self.addEventListener('activate', event => {  
+  event.waitUntil(
+    caches.keys()
+      .then(keyList => {
+        return Promise.all(keyList.map(key => {          
+          if (
+            key !== PRECACHE_NAME &&
+            key !== CAROUSEL_CACHE &&
+            key !== FONT_CACHE
+          ) {
+            console.log(key)
+            return caches.delete(key)
+          }
+        }))
+      })
+  )
+  return self.clients.claim()
 })
